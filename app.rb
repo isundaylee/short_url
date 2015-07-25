7c45c0a51e4378b60c8bea49d330d4971231e945
Jiahao Li <isundaylee.reg@gmail.com>
2015-07-24 23:02:43 -0700

    JSON REST interface.

---
+++ app.rb
@@ -0,0 +1,50 @@
+require 'bundler/setup'
+require 'sinatra/base'
+require 'json'
+
+require './url_store'
+
+class ShortURL < Sinatra::Base
+
+  configure do
+    @@store = URLStore.new
+  end
+
+  before do
+    content_type :json
+  end
+
+  helpers do
+    def base_url
+      @base_url ||= "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}"
+    end
+  end
+
+  post '/:name?' do
+    begin
+      name = @@store.create(params[:url], params[:name])
+      respond url: "#{base_url}/#{name}"
+    rescue URLStore::Exception => e
+      exception e
+    end
+  end
+
+  get '/:name' do
+    begin
+      respond actual_url: @@store.get(params[:name])
+    rescue URLStore::Exception => e
+      exception e
+    end
+  end
+
+  private
+
+    def respond(obj)
+      obj.to_json
+    end
+
+    def exception(e)
+      error 400, {error: e.message}.to_json
+    end
+
+end
---
+++ url_store.rb
@@ -0,0 +1,59 @@
+require 'redis'
+
+class URLStore
+
+  class Exception < StandardError; end
+
+  VALID_NAME_REGEX = /^[a-zA-Z0-9_-]+$/
+  VALID_GENERATED_CHARS = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
+  GENERATED_LENGTH = 2
+
+  def initialize
+    # reads configuration from ENV['REDIS_URL']
+    @redis = Redis.new
+  end
+
+  def self.valid_name?(name)
+    !!(VALID_NAME_REGEX =~ name)
+  end
+
+  def create(url, name)
+    raise Exception, 'Must provide a URL. ' if url.nil?
+    raise Exception, 'Invalid name. ' unless (name.nil? || self.class.valid_name?(name))
+
+    name ||= generate_name
+
+    result = @redis.setnx(redis_key(name), url)
+
+    raise Exception, 'Name already taken. ' unless result
+
+    name
+  end
+
+  def get(name)
+    raise Exception, 'Invalid name. ' if (name.nil? || !self.class.valid_name?(name))
+
+    url = @redis.get(redis_key(name))
+
+    raise Exception, 'Name does not exist. ' if url.nil?
+
+    url
+  end
+
+  private
+    def redis_key(name)
+      "urls:#{name}"
+    end
+
+    def generate_usable_name
+      while true do
+        name = generate_name
+        return name if @redis.get(name).nil?
+      end
+    end
+
+    def generate_name
+      (0...GENERATED_LENGTH).to_a.map { VALID_GENERATED_CHARS.sample }.join
+    end
+
+end
